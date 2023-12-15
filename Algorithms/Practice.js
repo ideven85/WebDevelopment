11a48e21e8cfa603c79e719403adc5a92d797270
Deven Kalra <deven@Macbook.local>
2023-12-16 00:29:55 +0530

    Removed Unncesaary files

---
+++ Algorithms/Practice.js
@@ -0,0 +1,20 @@
+new Promise((_, reject) => reject(new Error("Error 1")))
+    .then(value => console.log("Handler 1"))
+    .catch(reason => {
+        console.log("Caught Failure: " + reason)
+        return "Nothing"
+    })
+    .then(value => console.log("Handler 2 ", value))
+
+const promise = new Promise((resolve, reject) =>
+    reject(new Error("Error on first promise")))
+    .then(value => {
+        console.log("In first promise");
+        return "Then"
+    }).catch(reason => console.log("Error on second promise"))
+    .finally(console.log);
+
+console.log(promise.then(x => {
+    console.log(10);
+}).catch(reason => console.log("Bored of promises"))
+    .finally(console.log));
--- frontend/src/utitlties/FormatCurency.js
+++
@@ -1,8 +0,0 @@
-const CURRENCY_FORMATTER=
-    new Intl.NumberFormat(undefined,
-        {currency: "USD",style: "currency"});
-
-export function currencyFormatter(price) {
-    return CURRENCY_FORMATTER.format(price);
-
-}
