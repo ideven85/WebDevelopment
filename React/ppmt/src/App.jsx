8533e445236361dad1f181a07692d2c642b9e9e3
Deven Kalra <deven@Macbook.local>
2024-06-24 14:49:32 +0530

    Added Files Through Upload

--- React/ppmt/src/App.jsx
+++ React/ppmt/src/App.jsx
@@ -1,18 +1,29 @@
-
-import { Route,Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Home from './components/Home';
+// import { Route, Routes } from "react-router-dom";
+// import React from "react";
+// import Header from "./components/Header";
+// import Home from "./components/Home";
+// import img from './assets/coding.png';
 function App() {
- 
+  let score=0;
 
-  return (
-    <>
-    <Header />
-<Routes>
-<Route path="/" element={<Home />} />
-</Routes>
-    </>
+  const handleClick = (inc) => {
+    score+=inc;
+  }
+  console.log(score);
+  const returnScore = () => {
+    console.log(score)
+    return(
+        <><p>{score}</p></>
+    )
+  }
+  return(
+      <>
+    <button onClick={handleClick(1)}>{returnScore()}</button>
+      </>
   )
+
+
+
 }
 
-export default App
+export default App;
--- React/ppmt/src/main.jsx
+++ React/ppmt/src/main.jsx
@@ -7,8 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    
     <App />
-    </BrowserRouter>
+   
   </React.StrictMode>,
 )
